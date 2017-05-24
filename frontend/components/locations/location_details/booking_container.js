15150711e58fd0c57746d86c4657fd54ffc9981b
Tony Wang <sspy45@gmail.com>
2017-05-24 01:46:50 -0700

    added form for making bookings

--- frontend/components/locations/location_details/booking_container.js
+++ frontend/components/locations/location_details/booking_container.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import BookingFrom from './booking_form';
 import { createBooking } from '../../../actions/location_actions.js';
+import { fetchPets } from '../../../actions/user_actions.js';
+import { asArray } from '../../../reducers/selectors.js';
 
-const mapStateToProps = ({session}) => {
-  const errors = session.errors || [];
-  const currentUser = session.currentUser;
+const mapStateToProps = (state) => {
+  const errors = state.session.errors || [];
+  const currentUser = state.session.currentUser;
+  const pets = asArray(state.user_details.pets) || [];
   return {
     currentUser,
     errors,
+    pets
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    createBooking: (booking) => dispatch(createBooking(booking))
+    createBooking: (booking) => dispatch(createBooking(booking)),
+    fetchPets: (id) => dispatch(fetchPets(id))
   };
 };
 
--- frontend/components/locations/location_details/booking_form.jsx
+++ frontend/components/locations/location_details/booking_form.jsx
@@ -5,10 +5,36 @@ export default class BookingForm extends React.Component {
     super(props);
   }
 
-  render(){
+  componentWillMount(){
+    this.props.fetchPets(this.props.currentUser.id);
+  }
 
+  render(){
+    const {pets} = this.props;
+    let petList;
+    if(pets.length > 0){
+      debugger
+      petList = (
+        <select
+          value={pets[0].id}>
+          {pets.map(pet =>
+            <option value={pet.id}>
+              {pet.name}
+            </option>
+          )}
+        </select>
+      );
+    }
     return (
-      <div>hey</div>
+      <section>
+        <h2>
+          Make a Booking
+        </h2>
+        <p>CALENDAR</p>
+        <p>CALENDAR</p>
+        {petList}
+
+      </section>
     );
   }
 }
